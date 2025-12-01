9f99c096dff8ea7134e1e3fa471f5ed28cde7345
James Cooper <james@bitmechanic.com>
2025-12-01 15:30:50 -0800

    Fix lint errors by removing unused functions

--- slice.go
+++ slice.go
@@ -30,7 +30,3 @@ func Sample(slice interface{}) interface{} {
 	i := IntInRange(0, sliceReflectValue.Len()-1)
 	return sliceReflectValue.Index(i).Interface()
 }
-
-func anotherUnusedFunction() int {
-	return 99
-}
